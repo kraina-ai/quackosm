9f4c51f78390eb97280b5bb1853c2e29e2d78646
Kamil Raczycki <raczyckikamil@gmail.com>
2023-12-29 17:10:03 +0100

    fix: change optional imports for doctests

--- quackosm/conftest.py
+++ quackosm/conftest.py
@@ -1,5 +1,6 @@
 """Fixtures for doctests."""
 
+import sys
 import urllib.request
 from pathlib import Path
 
@@ -19,3 +20,44 @@ def add_pbf_files(doctest_namespace):  # type: ignore
         pbf_file_path = download_directory / f"{extract_name}.osm.pbf"
         urllib.request.urlretrieve(pbf_file_download_url, pbf_file_path)
         doctest_namespace[f"{extract_name}_pbf_path"] = pbf_file_path
+
+
+@pytest.fixture  # type: ignore
+def optional_packages() -> list[str]:
+    """Get a list with optional packages."""
+    return [
+        "rich",
+    ]
+
+
+@pytest.fixture(autouse=True)  # type: ignore
+def cleanup_imports():
+    """Clean imports."""
+    yield
+    sys.modules.pop("quackosm", None)
+
+
+class PackageDiscarder:
+    """Mock class for discarding list of packages."""
+
+    def __init__(self) -> None:
+        """Init mock class."""
+        self.pkgnames: list[str] = []
+
+    def find_spec(self, fullname, path, target=None) -> None:  # type: ignore
+        """Throws ImportError if matching module."""
+        if fullname in self.pkgnames:
+            raise ImportError()
+
+
+@pytest.fixture(autouse=True)  # type: ignore
+def no_optional_dependencies(monkeypatch, optional_packages):
+    """Mock environment without optional dependencies."""
+    d = PackageDiscarder()
+
+    for package in optional_packages:
+        sys.modules.pop(package, None)
+        d.pkgnames.append(package)
+    sys.meta_path.insert(0, d)
+    yield
+    sys.meta_path.remove(d)
