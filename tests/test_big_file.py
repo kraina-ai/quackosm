629a2e373c480c8ece4cc981b23f79f98c805186
Kamil Raczycki <raczyckikamil@gmail.com>
2024-01-06 15:09:16 +0100

    chore: add new tests for big files

--- tests/test_big_file.py
+++ tests/test_big_file.py
@@ -1,11 +1,21 @@
 """Test big file pipeline."""
+from pathlib import Path
+
+from parametrization import Parametrization as P
 from srai.loaders.download import download_file
 
 from quackosm.pbf_file_reader import PbfFileReader
 
 
-def test_big_file() -> None:
+@P.parameters("extract_name")  # type: ignore
+@P.case("Sweden", "sweden")  # type: ignore
+@P.case("Czech Republic", "czech-republic")  # type: ignore
+@P.case("Norway", "norway")  # type: ignore
+@P.case("Poland", "poland")  # type: ignore
+def test_big_file(extract_name: str) -> None:
     """Test if big file is working in a low memory environment."""
-    download_file("https://download.geofabrik.de/europe/sweden-latest.osm.pbf", "sweden.osm.pbf")
+    file_name = f"{extract_name}.osm.pbf"
+    download_file(f"https://download.geofabrik.de/europe/{extract_name}-latest.osm.pbf", file_name)
 
-    PbfFileReader().convert_pbf_to_gpq(pbf_path="sweden.osm.pbf", ignore_cache=True)
+    PbfFileReader().convert_pbf_to_gpq(pbf_path=file_name, ignore_cache=True)
+    Path(file_name).unlink()
