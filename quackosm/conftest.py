97ad1dee41b4306933d6ac4c5254a92c10b3a921
Kamil Raczycki <raczyckikamil@gmail.com>
2024-07-07 22:02:15 +0200

    fix: change doctests after changes to extracts

--- quackosm/conftest.py
+++ quackosm/conftest.py
@@ -6,7 +6,10 @@ import urllib.request
 from doctest import OutputChecker
 from pathlib import Path
 
+import duckdb
 import pytest
+
+from quackosm.osm_extracts.geofabrik import _get_geofabrik_index
 
 IGNORE_RESULT = doctest.register_optionflag("IGNORE_RESULT")
 
@@ -33,10 +36,21 @@ def add_pbf_files(doctest_namespace):  # type: ignore
     """Download PBF files used in doctests."""
     download_directory = Path("files")
     download_directory.mkdir(parents=True, exist_ok=True)
+
+    geofabrik_index = _get_geofabrik_index()
     for extract_name in EXTRACTS_NAMES:
         pbf_file_download_url = LFS_DIRECTORY_URL + f"{extract_name}-latest.osm.pbf"
         pbf_file_path = download_directory / f"{extract_name}.osm.pbf"
-        geofabrik_pbf_file_path = download_directory / f"Geofabrik_{extract_name}.osm.pbf"
+        geofabrik_download_path = geofabrik_index[geofabrik_index["id"] == extract_name].iloc[0][
+            "file_name"
+        ]
+        geofabrik_pbf_file_path = download_directory / f"{geofabrik_download_path}.osm.pbf"
         urllib.request.urlretrieve(pbf_file_download_url, pbf_file_path)
         doctest_namespace[f"{extract_name}_pbf_path"] = pbf_file_path
         shutil.copy(pbf_file_path, geofabrik_pbf_file_path)
+
+
+@pytest.fixture(autouse=True)
+def install_spatial_extension():  # type: ignore
+    """Install duckdb spatial extension."""
+    duckdb.install_extension("spatial")
