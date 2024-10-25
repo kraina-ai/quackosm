bc6001a9aa9686e191743912debeee951c175d1f
Kraina CI/CD <150701114+kraina-cicd@users.noreply.github.com>
2024-10-25 23:02:51 +0200

    chore(CI/CD): bump version 0.11.2 -> 0.11.3 (#175)
    * chore(CI/CD): bump version 0.11.2 -> 0.11.3
    
    * docs: update CHANGELOG.md
    
    * fix: add automatic cache directory creation
    
    ---------
    
    Co-authored-by: Kamil Raczycki <raczyckikamil@gmail.com>

--- quackosm/__init__.py
+++ quackosm/__init__.py
@@ -21,7 +21,7 @@ from quackosm.osm_extracts import display_available_extracts
 from quackosm.pbf_file_reader import PbfFileReader
 
 __app_name__ = "QuackOSM"
-__version__ = "0.11.2"
+__version__ = "0.11.3"
 
 __all__ = [
     "PbfFileReader",
