9fc759689ff95f090ea76550b3d0913825a162ad
Kamil Raczycki <raczyckikamil@gmail.com>
2025-01-03 00:49:34 +0100

    ci: change conftest fixture order

--- quackosm/conftest.py
+++ quackosm/conftest.py
@@ -36,36 +36,6 @@ EXTRACTS_NAMES = ["monaco", "kiribati", "maldives"]
 
 
 @pytest.fixture(autouse=True, scope="session")
-def add_pbf_files(doctest_namespace):  # type: ignore
-    """Download PBF files used in doctests."""
-    download_directory = Path("files")
-    download_directory.mkdir(parents=True, exist_ok=True)
-
-    geofabrik_index = _get_geofabrik_index()
-    for extract_name in EXTRACTS_NAMES:
-        pbf_file_download_url = LFS_DIRECTORY_URL + f"{extract_name}-latest.osm.pbf"
-        pbf_file_path = download_directory / f"{extract_name}.osm.pbf"
-        geofabrik_download_path = geofabrik_index[geofabrik_index["name"] == extract_name].iloc[0][
-            "file_name"
-        ]
-        geofabrik_pbf_file_path = download_directory / f"{geofabrik_download_path}.osm.pbf"
-        retrieve(
-            pbf_file_download_url,
-            fname=f"{extract_name}.osm.pbf",
-            path=download_directory,
-            progressbar=not FORCE_TERMINAL,
-            known_hash=None,
-        )
-        doctest_namespace[f"{extract_name}_pbf_path"] = pbf_file_path
-        shutil.copy(pbf_file_path, geofabrik_pbf_file_path)
-        print(
-            pbf_file_path,
-            geofabrik_pbf_file_path,
-            f"{extract_name}_pbf_path",
-        )
-
-
-@pytest.fixture(autouse=True, scope="session")
 def download_osm_extracts_indexes():  # type: ignore
     """Download OSM extract indexes files to cache."""
     download_directory = Path("cache")
@@ -88,6 +58,31 @@ def download_osm_extracts_indexes():  # type: ignore
             progressbar=not FORCE_TERMINAL,
             known_hash=None,
         )
+
+
+@pytest.fixture(autouse=True, scope="session")
+def add_pbf_files(doctest_namespace, download_osm_extracts_indexes):  # type: ignore
+    """Download PBF files used in doctests."""
+    download_directory = Path("files")
+    download_directory.mkdir(parents=True, exist_ok=True)
+
+    geofabrik_index = _get_geofabrik_index()
+    for extract_name in EXTRACTS_NAMES:
+        pbf_file_download_url = LFS_DIRECTORY_URL + f"{extract_name}-latest.osm.pbf"
+        pbf_file_path = download_directory / f"{extract_name}.osm.pbf"
+        geofabrik_download_path = geofabrik_index[geofabrik_index["name"] == extract_name].iloc[0][
+            "file_name"
+        ]
+        geofabrik_pbf_file_path = download_directory / f"{geofabrik_download_path}.osm.pbf"
+        retrieve(
+            pbf_file_download_url,
+            fname=f"{extract_name}.osm.pbf",
+            path=download_directory,
+            progressbar=not FORCE_TERMINAL,
+            known_hash=None,
+        )
+        doctest_namespace[f"{extract_name}_pbf_path"] = pbf_file_path
+        shutil.copy(pbf_file_path, geofabrik_pbf_file_path)
 
 
 @pytest.fixture(autouse=True, scope="session")
