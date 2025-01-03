c3f4e4521fb64d6ae16886486c8a8974e599e7e0
Kamil Raczycki <raczyckikamil@gmail.com>
2025-01-03 11:49:29 +0100

    ci: remove force terminal mode from tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,4 +7,3 @@ from pytest import Item
 def pytest_runtest_setup(item: Item) -> None:
     """Setup python encoding before `pytest_runtest_call(item)`."""
     os.environ["PYTHONIOENCODING"] = "utf-8"
-    os.environ["FORCE_TERMINAL_MODE"] = "true"
