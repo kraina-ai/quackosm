4816766fc39ab360b125f73a53608bc0dfa05fd7
Kraina CI/CD <150701114+kraina-cicd@users.noreply.github.com>
2025-07-18 23:40:00 +0200

    chore(CI/CD): bump version 0.15.2 -> 0.15.3 (#238)
    * chore(CI/CD): bump version 0.15.2 -> 0.15.3
    
    * docs: update CHANGELOG.md
    
    * ci: remove no_cross_platform tag
    
    * ci: change pylock installation with uv
    
    * ci: change uv installation
    
    * chore: test uv pip sync
    
    * ci: test other installation path
    
    * chore: change pyproject format
    
    * chore: change installation with uv
    
    ---------
    
    Co-authored-by: Kamil Raczycki <raczyckikamil@gmail.com>

--- quackosm/__init__.py
+++ quackosm/__init__.py
@@ -21,7 +21,7 @@ from quackosm.osm_extracts import display_available_extracts
 from quackosm.pbf_file_reader import PbfFileReader
 
 __app_name__ = "QuackOSM"
-__version__ = "0.15.2"
+__version__ = "0.15.3"
 
 __all__ = [
     "PbfFileReader",
