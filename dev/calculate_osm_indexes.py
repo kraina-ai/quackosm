d0e481112a34a5ec928c53857bddfa77cab2d58d
Kamil Raczycki <raczyckikamil@gmail.com>
2025-11-24 14:31:07 +0100

    feat: add precalculated indexes calculation logic

---
+++ dev/calculate_osm_indexes.py
@@ -0,0 +1,22 @@
+"""Recalculate OSM indexes and copy them to dedicated location."""
+
+from pathlib import Path
+
+from quackosm.osm_extracts import _get_combined_index, clear_osm_index_cache
+from quackosm.osm_extracts.extract import OsmExtractSource, _get_global_cache_file_path
+
+if __name__ == "__main__":
+    clear_osm_index_cache()
+    indexes = _get_combined_index()
+
+    extract_sources = [_source for _source in OsmExtractSource if _source != OsmExtractSource.any]
+
+    for extract_source in extract_sources:
+        cache_path = _get_global_cache_file_path(extract_source)
+        destination_path = (
+            Path(__file__).parent.parent
+            / "precalculated_indexes"
+            / f"{extract_source.value.lower()}_index.geojson"
+        )
+        print(f"Copying cache file {cache_path} to {destination_path}.")
+        destination_path.write_bytes(cache_path.read_bytes())
