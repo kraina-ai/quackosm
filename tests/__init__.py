2cf7cb072cc62ea871c1cabbfffbd4aec1c5f1e1
Kamil Raczycki <raczyckikamil@gmail.com>
2025-11-25 20:11:12 +0100

    fix: move geocode cache copying before loading tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,19 @@
 """Tests for QuackOSM module."""
+
+import shutil
+from pathlib import Path
+
+
+# Copy cache before loading tests
+def copy_geocode_cache() -> None:
+    """Load cached geocoding results."""
+    existing_cache_directory = Path(__file__).parent / "test_files" / "geocoding_cache"
+    geocoding_cache_directory = Path("cache")
+    geocoding_cache_directory.mkdir(exist_ok=True)
+    for file_path in existing_cache_directory.glob("*.json"):
+        destination_path = geocoding_cache_directory / file_path.name
+        shutil.copy(file_path, destination_path)
+        print(f"Copied {file_path} to {destination_path}")
+
+
+copy_geocode_cache()
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,10 @@
 """Global conftest for QuackOSM tests."""
 
 import os
-import shutil
-from pathlib import Path
 
-import pytest
 from pytest import Item
 
 
 def pytest_runtest_setup(item: Item) -> None:
     """Setup python encoding before `pytest_runtest_call(item)`."""
     os.environ["PYTHONIOENCODING"] = "utf-8"
-
-
-@pytest.fixture(autouse=True, scope="session")  # type: ignore
-def copy_geocode_cache() -> None:
-    """Load cached geocoding results."""
-    existing_cache_directory = Path(__file__).parent / "test_files" / "geocoding_cache"
-    geocoding_cache_directory = Path("cache")
-    geocoding_cache_directory.mkdir(exist_ok=True)
-    for file_path in existing_cache_directory.glob("*.json"):
-        destination_path = geocoding_cache_directory / file_path.name
-        shutil.copy(file_path, destination_path)
-        print(f"Copied {file_path} to {destination_path}")
