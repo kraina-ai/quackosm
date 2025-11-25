07ca7f24999b510cd21777999d281360a238ed0b
Kamil Raczycki <raczyckikamil@gmail.com>
2025-11-25 22:01:21 +0100

    chore(CI/CD): bump version 0.16.3 -> 0.16.4 (#269)
    * chore(CI/CD): bump version 0.16.3 -> 0.16.4
    
    * docs: update CHANGELOG.md
    
    * fix: remove macos-13 from workflows
    
    * ci: add debug message for tests
    
    * fix: move geocode cache copying before loading tests
    
    * fix: move geocode cache copying before loading tests
    
    * fix: move geocode cache copying before loading tests
    
    ---------
    
    Co-authored-by: Kraina CI/CD <cicd@kraina.ai>

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,16 +4,10 @@ import os
 import shutil
 from pathlib import Path
 
-import pytest
 from pytest import Item
 
 
-def pytest_runtest_setup(item: Item) -> None:
-    """Setup python encoding before `pytest_runtest_call(item)`."""
-    os.environ["PYTHONIOENCODING"] = "utf-8"
-
-
-@pytest.fixture(autouse=True, scope="session")  # type: ignore
+# Copy cache before loading tests
 def copy_geocode_cache() -> None:
     """Load cached geocoding results."""
     existing_cache_directory = Path(__file__).parent / "test_files" / "geocoding_cache"
@@ -22,3 +16,12 @@ def copy_geocode_cache() -> None:
     for file_path in existing_cache_directory.glob("*.json"):
         destination_path = geocoding_cache_directory / file_path.name
         shutil.copy(file_path, destination_path)
+        print(f"Copied {file_path} to {destination_path}")
+
+
+def pytest_runtest_setup(item: Item) -> None:
+    """Setup python encoding before `pytest_runtest_call(item)`."""
+    os.environ["PYTHONIOENCODING"] = "utf-8"
+
+
+copy_geocode_cache()
