2c1fca8f37d9c2df1966bdc8da1f0d198f00df1d
Kamil Raczycki <raczyckikamil@gmail.com>
2025-11-25 20:21:33 +0100

    fix: move geocode cache copying before loading tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,27 @@
 """Global conftest for QuackOSM tests."""
 
 import os
+import shutil
+from pathlib import Path
 
 from pytest import Item
+
+
+# Copy cache before loading tests
+def copy_geocode_cache() -> None:
+    """Load cached geocoding results."""
+    existing_cache_directory = Path(__file__).parent / "test_files" / "geocoding_cache"
+    geocoding_cache_directory = Path("cache")
+    geocoding_cache_directory.mkdir(exist_ok=True)
+    for file_path in existing_cache_directory.glob("*.json"):
+        destination_path = geocoding_cache_directory / file_path.name
+        shutil.copy(file_path, destination_path)
+        print(f"Copied {file_path} to {destination_path}")
 
 
 def pytest_runtest_setup(item: Item) -> None:
     """Setup python encoding before `pytest_runtest_call(item)`."""
     os.environ["PYTHONIOENCODING"] = "utf-8"
+
+
+copy_geocode_cache()
