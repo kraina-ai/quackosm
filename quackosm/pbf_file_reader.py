e8fdbc4ac237691fe191e5bd7eda1fea9f061204
Kamil Raczycki <raczyckikamil@gmail.com>
2026-05-20 11:45:12 +0200

    fix: change temporal import

--- quackosm/pbf_file_reader.py
+++ quackosm/pbf_file_reader.py
@@ -1,3 +1,3 @@
-from pbf_file_reader_old import PbfFileReader, _is_url_path
+from quackosm.pbf_file_reader_old import PbfFileReader, _is_url_path
 
-__all__ = ['PbfFileReader', '_is_url_path']
+__all__ = ["PbfFileReader", "_is_url_path"]
