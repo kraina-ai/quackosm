7223fd5cb213ea4956d36395f61cf076ca8d9433
Kamil Raczycki <raczyckikamil@gmail.com>
2026-05-20 16:17:30 +0200

    fix: chnage directory name for filtered valid intersected ways

--- quackosm/pbf_file_reader.py
+++ quackosm/pbf_file_reader.py
@@ -1,5 +1,6 @@
 """PbfFileReader mockup."""
 
-from quackosm.pbf_file_reader_old import PbfFileReader, _is_url_path, _run_in_multiprocessing_pool
+# from quackosm.pbf_file_reader_old import PbfFileReader, _is_url_path, _run_in_multiprocessing_pool
+from quackosm.pbf_file_reader_new import PbfFileReader, _is_url_path, _run_in_multiprocessing_pool
 
 __all__ = ["PbfFileReader", "_is_url_path", "_run_in_multiprocessing_pool"]
