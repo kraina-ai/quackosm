5e2343bb354b9a71150c4b9a0cdcf8bfabee3e23
Kamil Raczycki <raczyckikamil@gmail.com>
2026-05-20 14:19:25 +0200

    fix: change pbf file reader imports

--- quackosm/pbf_file_reader.py
+++ quackosm/pbf_file_reader.py
@@ -1,3 +1,5 @@
-from quackosm.pbf_file_reader_old import PbfFileReader, _is_url_path
+"""PbfFileReader mockup."""
 
-__all__ = ["PbfFileReader", "_is_url_path"]
+from quackosm.pbf_file_reader_old import PbfFileReader, _is_url_path, _run_in_multiprocessing_pool
+
+__all__ = ["PbfFileReader", "_is_url_path", "_run_in_multiprocessing_pool"]
